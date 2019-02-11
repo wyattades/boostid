fec9b957c282bbf361ab9060796990be40dfea25
--global <--global>
2019-02-10 22:51:39 -0800

    up

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -4,4 +4,4 @@ set -e
 
 export JEST_PUPPETEER_CONFIG=./node_modules/boostid/test_lib/jest-puppeteer.config.js
 
-npx jest --ci=false --runInBand --no-watchman --config ./node_modules/boostid/test_lib/jest.config.js
+npx jest --color --ci=false --runInBand --no-watchman --config ./node_modules/boostid/test_lib/jest.config.js
