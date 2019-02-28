494121f9eee7d810c7c3654855b7029e8a9e6f1c
--global <--global>
2019-02-28 02:33:10 -0800

    store test results locally

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+TEST_RESULTS_DIR=__boostid_results__
+
+mkdir -p $TEST_RESULTS_DIR
+
 JEST_PUPPETEER_CONFIG=./node_modules/boostid/test_lib/jest-puppeteer.config.js ./node_modules/.bin/jest \
 --color --ci=false --runInBand --no-watchman --config ./node_modules/boostid/test_lib/jest.config.js \
---json --outputFile=boostid_results.json
+--json --outputFile=${TEST_RESULTS_DIR}/results.json
