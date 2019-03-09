b942626b15daec1c193007db5359a7672a932468
--global <--global>
2019-03-08 17:03:27 -0800

    test timeout

--- test_lib/setup-test.js
+++ test_lib/setup-test.js
@@ -1,8 +1,9 @@
 const path = require('path');
+// Required when running tests locally
 const { toMatchImageSnapshot } = require(path.join(process.cwd(), 'node_modules/jest-image-snapshot'));
 
 expect.extend({ toMatchImageSnapshot });
 
 
 // Default timeout for each test
-jest.setTimeout(20000);
+jest.setTimeout(30000);
